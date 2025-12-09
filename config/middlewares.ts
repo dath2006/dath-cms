1a22ebfae4e72ca38a047753a114cd1de4ae092b
Sathish Dath <sathishdutt0@gmail.com>
2025-12-10 01:12:30 +0530

    init

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,8 +5,18 @@ export default [
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::body',
+    config: {
+      jsonLimit: '30mb',
+      formLimit: '30mb',
+      textLimit: '30mb',
+      formidable: {
+        maxFileSize: 100 * 1024 * 1024  // 100mb
+      }
+    }
+  }
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,37 @@
-export default () => ({});
+export default ({ env }) => ({
+  "cloudinary-media-library": {
+    enabled: true,
+    config: {
+      cloudName: env("CLOUDINARY_NAME"),
+      apiKey: env("CLOUDINARY_KEY"),
+      encryptionKey: env("CLOUDINARY_ENCRYPTION_KEY"),
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  "users-permissions": {
+    config: {
+      providers: {
+        google: {
+          enabled: true,
+          clientId: env("GOOGLE_CLIENT_ID"),
+          clientSecret: env("GOOGLE_CLIENT_SECRET"),
+          redirectUri: `${env("PUBLIC_URL", "http://localhost:1337")}/api/connect/google/callback`,
+        },
+      },
+    },
+  },
+});
